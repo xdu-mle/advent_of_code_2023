3f3cff0b295044cf84d08e131fb165a3a4c41d5d
Xingzhong (Eric) Du <domainxz@gmail.com>
2023-12-09 17:21:11 +1100

    feat(day_09): add solutions for day 9

---
+++ day_09/run1.py
@@ -0,0 +1,21 @@
+def calculate_next(arr):
+    cnt = 1
+    diff = []
+    for i in range(1, len(arr)):
+        d = arr[i]-arr[i-1]
+        if diff and d == diff[-1]:
+            cnt += 1
+        diff.append(d)
+    if cnt == len(diff):
+        return arr[-1]+diff[-1]
+    else:
+        return arr[-1]+calculate_next(diff)
+
+res = 0
+
+with open('input.txt', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        arr = [int(v) for v in line.strip().split(' ')]
+        res += calculate_next(arr)
+
+print(res)
